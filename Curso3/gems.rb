c76d2c8f7e627c581aebed5bd1917e8eaef7750c
Daniel Silva <jkldaniel@gmail.com>
2023-06-13 21:40:43 -0300

    Nova aula

---
+++ Curso3/gems.rb
@@ -0,0 +1,17 @@
+#Depois da biblioteca instalada, é preciso dizer que vai usar essa lib
+require 'os'
+
+def meu_SO
+    if OS.windows?
+        "Windows"
+    elsif OS.linux?
+        "Linux"
+    elsif OS.mac?
+        "Mac"
+    else
+        "Não identifiquei nenhum sistema operacional"
+    end
+end
+
+puts "Meu PC é #{OS.bits} bits, possui #{OS.cpu_count} cores e o sistema operacional é #{meu_SO}"
+    
